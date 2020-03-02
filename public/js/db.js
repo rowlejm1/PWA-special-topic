ee94b79e2f0ccbe3acafbe52b54d0a5300eefbc9
Joe <ROWLEJM1@student.op.ac.nz>
2020-03-02 19:06:02 +1300

    finish tutorial

---
+++ public/js/db.js
@@ -0,0 +1,55 @@
+// offline data
+db.enablePersistence()
+    .catch(err => {
+        if(err.code == 'failed-precondition') {
+            // multiple tabs opened
+            console.log('persistence failed');
+        }
+        else if(err.code == 'unimplemented') {
+            // lack of browser support
+            console.log('persistence is not available');
+        }
+    });
+
+// real-time listener
+db.collection('datasets').onSnapshot((snapshot) => {
+    //console.log(snapshot.docChanges());
+    snapshot.docChanges().forEach(change => {
+        //console.log(change, change.doc.data(), change.doc.id);
+        if(change.type === 'added') {
+            // add doc data to web page
+            renderRecipe(change.doc.data(), change.doc.id);
+        }
+        if(change.type === 'removed') {
+            // remove doc data from the web page
+            removeRecipe(change.doc.id);
+        }
+    });
+});
+
+// add new recipe
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const recipe = {
+        title: form.title.value,
+        ingredients: form.ingredients.value
+    };
+
+    db.collection('datasets').add(recipe)
+        .catch(err => console.log(err));
+
+    form.title.value = '';
+    form.ingredients.value = '';
+});
+
+// delete a recipe
+const recipeContainer = document.querySelector('.recipes');
+recipeContainer.addEventListener('click', evt => {
+    //console.log(evt);
+    if(evt.target.tagName === 'I') {
+        const id = evt.target.getAttribute('data-id');
+        db.collection('datasets').doc(id).delete();
+    }
+});
---
+++ public/sw.js
@@ -0,0 +1,72 @@
+const staticCacheName = 'site-static-v7';
+const dynamicCacheName = 'site-dynamic-v5';
+const assets = [
+    '/',
+    '/index.html',
+    '/pages/fallback.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v50/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size))
+            }
+        })
+    })
+}
+
+// install service worker
+self.addEventListener('install', evt => {
+    //console.log('service worker has been installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            return cache.addAll(assets);
+        })
+    );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+    //console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))    
+            )
+        })
+    );
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+    if(evt.request.url.indexOf('firestore.googleapis.com') === -1) {
+        evt.respondWith(
+            caches.match(evt.request).then(cacheRes => {
+                return cacheRes || fetch(evt.request).then(fetchRes => {
+                    return caches.open(dynamicCacheName).then(cache => {
+                        cache.put(evt.request.url, fetchRes.clone());
+                        limitCacheSize(dynamicCacheName, 30);
+                        return fetchRes;
+                    })
+                });
+            }).catch(() => {
+                if(evt.request.url.indexOf('.html') > -1){
+                    return caches.match('/pages/fallback.html');
+                }
+            })
+        );
+    }
+});
